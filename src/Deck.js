9e81171b310684a80be34f59231a548b2271b1f0
Matt Elhotiby <hotiby@gmail.com>
2018-12-09 15:46:34 -0500

    Adding card stacking and animation

--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import { View, Animated, PanResponder, Dimensions } from "react-native";
+import {
+  View,
+  Animated,
+  PanResponder,
+  Dimensions,
+  StyleSheet
+} from "react-native";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const SWIPE_THRESHOLD = 0.25 * Dimensions.get("window").width;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeLeft: () => {},
+    onSwipeRight: () => {}
+  };
+
   constructor(props) {
     super(props);
 
@@ -16,9 +28,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log("Liked ....");
+          this.forceSwipe("right");
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log("not Liked ....");
+          this.forceSwipe("left");
         } else {
           this.resetPosition(gesture);
         }
@@ -27,6 +39,29 @@ class Deck extends Component {
 
     this.panResponder = panResponder;
     this.position = position;
+
+    this.state = {
+      index: 0
+    };
+  }
+
+  forceSwipe(direction) {
+    const x = direction === "right" ? SCREEN_WIDTH + 200 : -SCREEN_WIDTH - 200;
+
+    Animated.timing(this.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeRight, onSwipeLeft, data } = this.props;
+    const item = data[this.state.index];
+
+    this.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
+
+    direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
   }
 
   resetPosition(gesture) {
@@ -48,12 +83,17 @@ class Deck extends Component {
   }
 
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards();
+    }
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) return null;
+
+      if (this.state.index === i) {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             {...this.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
@@ -61,13 +101,22 @@ class Deck extends Component {
         );
       }
 
-      return this.props.renderCard(item);
-    });
+      return (
+        <View style={styles.cardStyle}>{this.props.renderCard(item)}</View>
+      );
+    }).reverse();
   }
 
   render() {
     return <View>{this.renderCards()}</View>;
   }
 }
+
+const styles = StyleSheet.create({
+  cardStyle: {
+    position: "absolute",
+    width: SCREEN_WIDTH
+  }
+});
 
 export default Deck;
